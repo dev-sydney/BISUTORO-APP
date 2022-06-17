c3b1fdc1e0e1ac2a9b8071a07d904f257768e2e4
dev-sydney <sydneyotutey279@gmail.com>
2022-06-17 23:02:04 +0000

    Added some more types for adding meals, as well when theres an error

--- client/src/contexts/types.js
+++ client/src/contexts/types.js
@@ -1,4 +1,5 @@
 export const ADD_MEAL = 'ADD_MEAL';
+export const ADD_MEAL_ERROR = 'ADD_MEAL_ERROR';
 export const DELETE_MEAL = 'DELETE_MEAL';
 export const ADD_TO_ORDER = 'ADD_TO_ORDER';
 export const REMOVE_FROM_ORDERS = 'REMOVE_FROM_ORDERS';
