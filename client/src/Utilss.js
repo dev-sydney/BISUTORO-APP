1c0149319dc742d5ca5e56559717ebcd16700132
dev-sydney <sydneyotutey279@gmail.com>
2022-06-21 22:04:42 +0000

    contains some functions,objects,etc that will be reused throughout various modules

---
+++ client/src/Utilss.js
@@ -0,0 +1,16 @@
+//TODO: Refactor the config
+export const config = {
+  headers: {
+    'Content-type': 'application/json',
+  },
+};
+
+export const asyncActions = (fn, type, dispatch) => () => {
+  fn().catch((err) => {
+    console.log(err);
+    dispatch({
+      type,
+      payload: err.response.data.message,
+    });
+  });
+};
