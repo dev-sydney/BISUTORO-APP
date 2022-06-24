2c6e28ad07d3eb21031afc8207781cae72c983b8
dev-sydney <sydneyotutey279@gmail.com>
2022-06-24 22:01:56 +0000

    Added the button for making reviews,

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,4 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+
+import ReviewContainer from './ReviewContainer';
 import './../styles/ModalStyle.scss';
 
 import productContext from './../contexts/ProductContext';
@@ -6,6 +8,7 @@ import productContext from './../contexts/ProductContext';
 const Modal = ({ setIsModalOpen }) => {
   const mealsContext = useContext(productContext);
   const { currentMeal, addToOrder } = mealsContext;
+  const [reviewBlock, setReviewBlock] = useState(false);
 
   const onClick = () => {
     setIsModalOpen(false);
@@ -17,13 +20,32 @@ const Modal = ({ setIsModalOpen }) => {
     <div className="modal_bg">
       <button onClick={onClick}>❌</button>
       <div className="md_window">
-        <div>
-          <img src={`/img/meals/${currentMeal.image}`} alt={currentMeal.name} />
-          <div>{currentMeal.name}</div>
-          <div>{currentMeal.serving} g</div>
-          <div>${currentMeal.price}</div>
-          <button onClick={onAddOrder}>Add To Order</button>
-        </div>
+        {!reviewBlock ? (
+          <div>
+            <img
+              src={`/img/meals/${currentMeal.image}`}
+              alt={currentMeal.name}
+            />
+            <div>{currentMeal.name}</div>
+            <div>{currentMeal.serving} g</div>
+            <div>${currentMeal.price}</div>
+
+            <div
+              onClick={() => {
+                setReviewBlock(!reviewBlock);
+              }}
+            >
+              <span>⭐</span>
+              <span>⭐</span>
+              <span>⭐</span>
+              <span>How about a review ?</span>
+            </div>
+
+            <button onClick={onAddOrder}>Add To Order</button>
+          </div>
+        ) : (
+          <ReviewContainer setReviewBlock={setReviewBlock} />
+        )}
       </div>
     </div>
   );
