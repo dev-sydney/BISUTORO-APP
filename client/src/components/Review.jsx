30615e5467226fe0e8753fd75190f469182d9731
dev-sydney <sydneyotutey279@gmail.com>
2022-06-24 22:03:05 +0000

    Added a new component for rendering out user reviews and comments

---
+++ client/src/components/Review.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './../styles/reviewStyle.scss';
+const Review = () => {
+  return (
+    <div className="review">
+      <div className="profile">
+        <p>PP</p>
+      </div>
+
+      <div className="details">
+        <span>⭐</span>
+        <span>⭐</span>
+        <span>⭐</span>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque </p>
+        <p>27 sept 2022</p>
+      </div>
+    </div>
+  );
+};
+
+export default Review;
