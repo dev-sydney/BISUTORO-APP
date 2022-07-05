fbae372a98a85d98148c2d11ca46d76c309e79c2
dev-sydney <sydneyotutey279@gmail.com>
2022-07-05 20:21:13 +0000

    Added the routes for meal orders,and implemented cors as well

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
+const path = require('path');
 //3RD PARTY MODULES
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 //CUSTOM MODULES
 const mealRouter = require('./routes/mealRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const orderRouter = require('./routes/orderRoutes');
 
 const CustomError = require('./utils/customError');
 const errorController = require('./controllers/errorController');
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
+app.use(express.static(path.join(__dirname, '/client/public')));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
@@ -20,6 +24,7 @@ app.use(cookieParser());
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/meals', mealRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/orders', orderRouter);
 
 app.all('*', (req, res, next) => {
   next(
