201458f66a0988d2f14b24012017fb30f7aacbdb
dev-sydney <sydneyotutey279@gmail.com>
2022-07-05 20:34:00 +0000

    OrderMeals function gets the checkout session from the server and then redirects to the checkout page to charge the credit card

---
+++ client/src/js/stripe.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { config } from '../Utilss';
+import { loadStripe } from '@stripe/stripe-js';
+const stripe = loadStripe(
+  'pk_test_51LHFLYCzMWGZTWYcO8piuiqgYo6Kj3dSdpihgTmnxEpsRvvbKH3Y1CpKhWGtGcvBgUMuB9vJ7hR82fzhTwuhKUgk00FwuueWL2'
+);
+export const OrderMeals = async (orders) => {
+  try {
+    //get the ids of the ordered meals
+    let mealIds = orders.map((el) => el._id).join(',');
+    //pass meal ids as queries + Get the chekout session from the API
+    const res = await axios.get(
+      `/api/v1/orders/checkout-session/?mealIds=${mealIds}`,
+      config
+    );
+    if (res.data.status === 'success') {
+      const { session } = res.data;
+      //Create the  chcekout form + change the credit card
+
+      stripe
+        .then((res) => {
+          res.redirectToCheckout({
+            sessionId: session.id,
+          });
+        })
+        .then((res) => {
+          if (res.error) {
+            console.log(res.error.message);
+          }
+        });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
