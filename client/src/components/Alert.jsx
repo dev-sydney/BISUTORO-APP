37d7974239905f3a565c8391219e78ea4ef9d6de
dev-sydney <sydneyotutey279@gmail.com>
2022-07-06 17:57:17 +0000

    Added some conditonal classNames for when the alert is a n error or success alert

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -5,13 +5,17 @@ import './../styles/alertStyle.scss';
 
 const Alert = ({ setIsAlertOpen }) => {
   const authContxt = useContext(AuthContext);
-  let { authMsg, clearAuthMsg } = authContxt;
+  let { authMsg, clearAuthMsg, authMsgType } = authContxt;
   useEffect(() => {
     authMsg && setIsAlertOpen(true);
     //eslint-disable-next-line
   }, [authMsg]);
   return (
-    <div className="alert">
+    <div
+      className={`alert ${authMsg ? 'show' : 'hidden'} ${
+        authMsg ? 'success' : 'error'
+      }`}
+    >
       {authMsg ? authMsg : ''}
       <button
         onClick={() => {
