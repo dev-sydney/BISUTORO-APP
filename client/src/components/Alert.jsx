a365ff788d32eba65b11d618c607afd3d1d61426
dev-sydney <sydneyotutey279@gmail.com>
2022-07-11 12:18:12 +0000

    Implemented the condtional rendering for the alert message, as well as the conditional classes

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -1,25 +1,32 @@
 import React, { useContext, useEffect } from 'react';
 import AuthContext from '../contexts/AuthContext';
+import productContext from './../contexts/ProductContext';
 
 import './../styles/alertStyle.scss';
 
 const Alert = ({ setIsAlertOpen }) => {
   const authContxt = useContext(AuthContext);
+  const productContxt = useContext(productContext);
+
   let { authMsg, clearAuthMsg, authMsgType } = authContxt;
+  let { alertMsg, alertMsgType, removeAlerts } = productContxt;
+
   useEffect(() => {
     authMsg && setIsAlertOpen(true);
     //eslint-disable-next-line
-  }, [authMsg]);
+  }, [authMsg, alertMsg]);
   return (
     <div
-      className={`alert ${authMsg ? 'show' : 'hidden'} ${
-        authMsg ? 'success' : 'error'
+      className={`alert ${authMsg || alertMsg ? 'show' : 'hidden'} ${
+        authMsgType || alertMsgType ? 'success' : 'error'
       }`}
     >
-      {authMsg ? authMsg : ''}
+      {authMsg && authMsg}
+      {alertMsg && alertMsg}
       <button
         onClick={() => {
           clearAuthMsg();
+          removeAlerts();
         }}
       >
         ❌
