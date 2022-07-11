620dc3e6c426d557c2c7437fb6466e4e8f343152
dev-sydney <sydneyotutey279@gmail.com>
2022-07-11 12:09:09 +0000

    Relocated the login and logout button to the left pane

--- client/src/components/layouts/Sidebar.jsx
+++ client/src/components/layouts/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import AuthContext from '../../contexts/AuthContext';
 import './../../styles/sideBarStyle.scss';
@@ -8,36 +9,60 @@ const Sidebar = () => {
   const authContxt = useContext(AuthContext);
   const { loggedInUser } = authContxt;
 
+  const navigate = useNavigate();
+
+  const { isAuthenticated, asyncAuthActions } = authContxt;
+
   return (
     <div className="left_side">
-      <div>
-        <NavLink to="/" className="links">
-          OVERVIEW
-        </NavLink>
+      <img
+        className="profile"
+        src="/img/users/user-629d35ebaee820196aa455b3-1655325780778.jpeg"
+      />
+      <h2>Hello Sydney</h2>
+      <div className={`add_meal_btn `}>CREATE NEW MEAL +</div>
+      <p>Menu</p>
+      <div className={`links`}>
+        <NavLink to="/">OVERVIEW</NavLink>
       </div>
-      <div>
-        <NavLink to="/favourites" className="links">
-          FAVORITES
-        </NavLink>
+      <div className={`links`}>
+        <NavLink to="/favourites">FAVORITES</NavLink>
       </div>
-      <div>
-        <NavLink to="/me" className="links">
-          PROFILE
-        </NavLink>
+      <div className={`links`}>
+        <NavLink to="/me">PROFILE</NavLink>
       </div>
-      <div>
-        <NavLink to="/faq" className="links">
-          FAQ
-        </NavLink>
+      <div className={`links`}>
+        <NavLink to="/order-history">ORDER HISTORY</NavLink>
+      </div>
+      <div className={`links`}>
+        <NavLink to="/faq">FAQ</NavLink>
       </div>
       {loggedInUser && loggedInUser.role === 'manager' ? (
-        <div>
+        <div className={`links`}>
           <NavLink to="/dashboard" className={'links'}>
             DASHBOARD
           </NavLink>
         </div>
       ) : (
         ''
+      )}
+
+      {!isAuthenticated ? (
+        <div>
+          <NavLink to="/signup">
+            <button>SIGN UP</button>
+          </NavLink>
+
+          <button>LOGIN</button>
+        </div>
+      ) : (
+        <button
+          onClick={() => {
+            asyncAuthActions.logout(navigate);
+          }}
+        >
+          LOG OUT
+        </button>
       )}
     </div>
   );
