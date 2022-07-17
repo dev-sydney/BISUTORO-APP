397205b0ea2a3b300e86ad863b1d66fe87c94dc9
dev-sydney <sydneyotutey279@gmail.com>
2022-07-17 21:17:21 +0000

    got rid of sidebar on the left and replaced it with a header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
+import { useState } from 'react';
 ////////////////////3RD PARTY IMPORTS
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 /////////////////////////STYLE IMPORTS
 import './App.css';
 import './styles/containerStyle.scss';
 ///////////////////COMPONENTS IMPORTS
-import Sidebar from './components/layouts/Sidebar';
+import Header from './components/layouts/Header';
 import Home from './components/layouts/Home';
 import Profile from './components/Profile';
 import Login from './components/auth/Login';
@@ -12,19 +13,27 @@ import Signup from './components/auth/Signup';
 import MealForm from './components/MealForm';
 import Dashboard from './components/Dashboard';
 import Favourites from './components/Favourites';
+import Modal from './components/Modal';
 //////////////////CONTEXT IMPORTS
 import { MealContextProvider } from './contexts/ProductContext';
 import { AuthProvider } from './contexts/AuthContext';
 function App() {
+  const [isModal, setIsModal] = useState(false);
   return (
     <div className="App">
-      <div className="flex_container">
-        <AuthProvider>
-          <MealContextProvider>
-            <BrowserRouter>
-              <Sidebar />
+      <AuthProvider>
+        <MealContextProvider>
+          <BrowserRouter>
+            <div /* className="flex_container" */ style={{ height: '100%' }}>
+              <Modal setIsModal={setIsModal} isModal={isModal} />
+
+              <Header />
               <Routes>
-                <Route exact path="/" element={<Home />} />
+                <Route
+                  exact
+                  path="/"
+                  element={<Home setIsModal={setIsModal} isModal={isModal} />}
+                />
                 <Route exact path="/me" element={<Profile />} />
                 <Route exact path="/login" element={<Login />} />
                 <Route exact path="/signup" element={<Signup />} />
@@ -36,10 +45,10 @@ function App() {
                 />
                 <Route exact path="/favourites" element={<Favourites />} />
               </Routes>
-            </BrowserRouter>
-          </MealContextProvider>
-        </AuthProvider>
-      </div>
+            </div>
+          </BrowserRouter>
+        </MealContextProvider>
+      </AuthProvider>
     </div>
   );
 }
