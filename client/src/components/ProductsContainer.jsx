39ac28e581536112d9e7792d6cdde2e4087592c3
dev-sydney <sydneyotutey279@gmail.com>
2022-07-22 19:59:18 +0000

    Implemented conditional rendering to display the skeletion loading component in between data fetching, and got rid of some unused state

--- client/src/components/ProductsContainer.jsx
+++ client/src/components/ProductsContainer.jsx
@@ -3,6 +3,8 @@ import React, { Fragment, useContext, useState, useEffect } from 'react';
 import ProductContext from '../contexts/ProductContext';
 
 import Product from './Product';
+import SkeletonProduct from './SkeletonProduct';
+
 import './../styles/containerStyle.scss';
 
 const ProductsControl = () => {
@@ -23,17 +25,37 @@ const ProductsControl = () => {
     <Fragment>
       <div className="product_container">
         {meals &&
-          meals.map((el) => (
-            <Product
-              meal={el}
-              key={el._id}
-              setIsModalOpen={setIsModalOpen}
-              isModalOpen={isModalOpen}
-              isFavouritesPage={isFavouritesPage}
-            />
+          (meals.length === 0 ? (
+            <h2>No meals available at the moment</h2>
+          ) : (
+            meals.map((el) => (
+              <Product
+                meal={el}
+                key={el._id}
+                setIsModalOpen={setIsModalOpen}
+                isModalOpen={isModalOpen}
+                isFavouritesPage={isFavouritesPage}
+              />
+            ))
           ))}
+        {!meals &&
+          [1, 2, 3, , 4, 5, 6].map((el) => <SkeletonProduct key={el} />)}
       </div>
     </Fragment>
   );
 };
 export default ProductsControl;
+// {meals &&
+//   }
+
+// {!meals
+//   ? [1, 2, 3, 4, 5, 6].map((el) => <SkeletonProduct key={el} />)
+//   : meals.map((el) => (
+//       <Product
+//         meal={el}
+//         key={el._id}
+//         setIsModalOpen={setIsModalOpen}
+//         isModalOpen={isModalOpen}
+//         isFavouritesPage={isFavouritesPage}
+//       />
+//     ))}
